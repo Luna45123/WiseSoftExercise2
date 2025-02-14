f59f0863a308faaf2024f44cc4b05cb279c99dd3
Sunny4826 <118628971+Luna45123@users.noreply.github.com>
2025-02-14 19:30:16 +0700

    fix file input

--- src/com/wiseSoft/seminar/Main.java
+++ src/com/wiseSoft/seminar/Main.java
@@ -9,7 +9,6 @@ import com.wiseSoft.seminar.service.SeminarSchedulerService;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
